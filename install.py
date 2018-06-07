7da5427f42e9a5d097be2c540fa3ddb05a83626e
Unknown <b6010572@my.shu.ac.uk>
2018-06-07 15:14:54 +0000

    Installed CodeIgniter

--- install.py
+++ install.py
@@ -1,8 +1,19 @@
-# Installs CodeIgniter to the current Directory
-
+# Installs CodeIgniter to the current directory
 import urllib
+import os
+import zipfile
 
 url = 'https://github.com/bcit-ci/CodeIgniter/archive/3.1.8.zip'
-file_name = 'CodeIgniter.zip'
+zip_file = {
+    'name': 'temp.zip'
+}
 
-urllib.urlretrieve(url, file_name)
+urllib.urlretrieve(url, zip_file['name'])
+
+# Extract the zip file to the current folder
+zip_file['instance'] = zipfile.ZipFile(zip_file['name'], 'r')
+zip_file['instance'].extractall()
+zip_file['instance'].close()
+
+# Delete the downloaded zip file
+os.remove(zip_file['name'])
