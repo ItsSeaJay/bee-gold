adc88e201da7afaa3263e8c7e1937ea1a24546e8
Unknown <b6010572@my.shu.ac.uk>
2018-06-08 19:29:02 +0000

    Creating controllers now takes to config into account

--- utilities/create_controller.py
+++ utilities/create_controller.py
@@ -1,4 +1,5 @@
 # Adds a new controller to a CodeIgniter Installation
+import json
 import os
 import sys
 import string
@@ -10,20 +11,24 @@ location = input('Enter the location of the new controller [\'Super\']: ') or 'S
 # Get the base name from the location
 base_name = os.path.basename(os.path.normpath(location))
 
+# Get the contents of the config file and convert it to a Python dictionary
+with open('config.json', 'r') as file:
+    config = json.loads(file.read())
+
 # Get the contents of the template and format it into a controller
 with open('templates/controller.template.php', 'r') as file:
     template = file.read()
     controller = template.format(name = base_name)
 
 # Write that controller to the appropriate folder
-base_name = 'CodeIgniter-' + version_number + '/application/controllers/' + os.path.dirname(location)
+base_name = config['paths']['install'] + 'application/controllers/' + os.path.dirname(location)
 
 # Make sure that the directories exist
 if not os.path.exists(base_name):
     os.makedirs(base_name)
 
 # Then write the file into that location
-file_path = 'CodeIgniter-' + version_number + '/application/controllers/' + location + '.php'
+file_path = config['paths']['install'] + 'application/controllers/' + location + '.php'
 
 with open(file_path, 'w') as file:
     file.write(controller)
